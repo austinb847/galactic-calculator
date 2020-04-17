7c8b751a9165680fee71562d963a4080aaad6b27
austinb847 <austinb847@icloud.com>
2020-04-17 10:31:13 -0700

    implement meaningful fail test for calcAgeEarthDays method, succesfull fail

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -4,8 +4,14 @@ import { Calculator } from './../src/calculator.js';
 describe('Calculator', () => {
 
   test('should correctly create a calculator object with one age input', () => {
-    let calculator = new Calculator(24);
-    expect(calculator.personAge).toEqual(24);
+    let calculator = new Calculator(20);
+    expect(calculator.personAge).toEqual(20);
+  });
+
+  test('should correctly calculate the age in earth days', () => {
+    let calculator = new Calculator(20);
+    let ageEarthDays = calculator.calcAgeEarthDays();
+    expect(ageEarthDays).toEqual(7300);
   });
 
 
