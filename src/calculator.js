77edc5892e339eb5d3df4c311a5ec82de972e047
austinb847 <austinb847@icloud.com>
2020-04-18 16:46:20 -0700

    configure webpack for images

--- src/calculator.js
+++ src/calculator.js
@@ -10,24 +10,24 @@ export class Calculator {
 
   calcAgeMercuryYears() {
     const earth_days_age = this.calcAgeEarthDays();
-    const age = earth_days_age / 88
+    const age = earth_days_age / 88;
     return this.roundNum(age); 
   }
 
   calcAgeVenusYears() {
     const earth_days_age = this.calcAgeEarthDays();
-    const age = earth_days_age / 225
+    const age = earth_days_age / 225;
     return this.roundNum(age);
   }
 
   calcAgeMarsYears() {
     const earth_days_age = this.calcAgeEarthDays();
-    const age = earth_days_age / 687
+    const age = earth_days_age / 687;
     return this.roundNum(age);
   }
 
   calcAgeJupiterYears() {
-    const age = this.personAge / 12
+    const age = this.personAge / 12;
     return this.roundNum(age);
   }
 
