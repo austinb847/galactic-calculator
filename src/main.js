23126fad3be4b8d7636a7263733b69edb3bb5586
austinb847 <austinb847@icloud.com>
2020-04-18 17:55:51 -0700

    refactor code

--- src/main.js
+++ src/main.js
@@ -5,6 +5,33 @@ import './styles.css';
 import { Calculator } from "./calculator.js";
 
 $(document).ready(function() {
+
+  const DOMSelectors = {
+    age_mercury: $("#mercAge"),
+    mercury_exp: $("#mercExpec"),
+    age_venus: $("#venuAge"),
+    venus_exp: $("#venuExpec"),
+    age_mars:  $("#marsAge"),
+    mars_exp: $("#marsExpec"),
+    age_jupiter: $("#jupiAge"),
+    jupiter_exp: $("#jupiExpec"),
+  };
+
+  function showContent(obj) {
+    DOMSelectors.age_mercury.text(obj.ageMercuryYears);
+    DOMSelectors.mercury_exp.text(obj.calcYearsLeft(obj.ageMercuryYears));
+    
+    DOMSelectors.age_venus.text(obj.ageVenusYears);
+    DOMSelectors.venus_exp.text(obj.calcYearsLeft(obj.ageVenusYears));
+
+    DOMSelectors.age_mars.text(obj.ageMarsYears);
+    DOMSelectors.mars_exp.text(obj.calcYearsLeft(obj.ageMarsYears));
+
+    DOMSelectors.age_jupiter.text(obj.ageJupiterYears);
+    DOMSelectors.jupiter_exp.text(obj.calcYearsLeft(obj.ageJupiterYears));
+
+  }
+
   $("#ageCalculator").submit(function(event) {
     event.preventDefault();
     let userAge = parseInt($("#userAge").val());
@@ -12,14 +39,8 @@ $(document).ready(function() {
     
     const calc = new Calculator(userAge, userExpectancy);
     calc.calcAllAges();
+    showContent(calc);
 
-    $("#mercAge").text(calc.ageMercuryYears);
-    $("#venuAge").text(calc.ageVenusYears);
-    $("#marsAge").text(calc.ageMarsYears);
-    $("#jupiAge").text(calc.ageJupiterYears);
   });
-
-
-
 
 });
